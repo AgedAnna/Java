95e9de50e4e7393433e634a8e8b169e6ad019ca0
Agedanna <annabeatryz12345@gmail.com>
2023-05-13 16:50:29 -0300

    atvd 6

--- Ficha_1/src/main/java/Atvd_6.java
+++ Ficha_1/src/main/java/Atvd_6.java
@@ -1,12 +1,43 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * 6) Escreva um programa que leia três valores inteiros e diferentes e 
+ *    mostre-os e ordem crescente.
  * 
  */
 
-/**
- *
- * @author annab
- */
+import java.util.Scanner;
 public class Atvd_6 {
+public static void main(String[] args){
     
+    int n1, n2, n3;
+    Scanner dados;
+    dados = new Scanner(System.in);
+    
+    System.out.println("Digite um número: ");
+    n1 = dados.nextInt();
+    
+    System.out.println("Digite um número: ");
+    n2 = dados.nextInt();
+    
+    System.out.println("Digite um número: ");
+    n3 = dados.nextInt();
+    
+    
+    if (n1<n2 && n1<n3 && n2<n3){
+        System.out.print(n1+""+n2+""+n3);
+    }
+    
+    else if (n2<n1 && n2<n3 && n1<n3){
+     System.out.print(n2+""+n1+""+n3);
+    }
+    
+    else if (n3<n1 && n3<n2 && n2<n1){
+     System.out.print(n3+""+n2+""+n1);
+    }
+    
+    else{
+     System.out.print(n3+""+n1+""+n2);
+    }
+    
+    }
 }
+
