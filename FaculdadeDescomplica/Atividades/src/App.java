8ecfc817bbf76ec5e9ded54edc5b15c8bf753636
Agedanna <annabeatryz12345@gmail.com>
2023-10-12 23:25:41 -0300

    descomplica

---
+++ FaculdadeDescomplica/Atividades/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Anna Beatryz");
+
+    }
+}
---
+++ FaculdadeDescomplica/Atividades/src/atv2.java
@@ -0,0 +1,14 @@
+public class atv2 {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Nome: ");
+        System.out.println("Anna");
+        System.out.println("Idade: ");
+        System.out.println(19);
+        System.out.println("Altura: ");
+        System.out.println(1.57);
+        System.out.println("Adulta: ");
+        System.out.println(false);
+    }
+    
+}
