65c989a80f4a7b7e3268c5a5106c71efefc43ee4
Anna Beatryz <106432032+AgedAnna@users.noreply.github.com>
2023-11-09 21:39:55 -0300

    Atividade 09/11

---
+++ Funcionario.java
@@ -0,0 +1,11 @@
+package sistemacadastro;
+
+public class Funcionario {
+    public String nome;
+    public double salario;
+    
+    public Funcionario(){
+        nome = "";
+        salario = 0.0;
+    }
+}
