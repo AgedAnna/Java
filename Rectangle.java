1c0030bf247bb346080ff5b91609062b7b525f9c
Anna Beatryz <106432032+AgedAnna@users.noreply.github.com>
2024-03-01 19:20:10 -0300

    POO
    Primeiras atvs em POO JAVA

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+    private double width;
+    private double heigth;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(double heigth) {
+        this.heigth = heigth;
+    }
+}
